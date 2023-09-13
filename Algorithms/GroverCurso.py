6c1626dd823821faafeeeae6722e4e0a6a5f8785
arturo <arturequejo@gmail.com>
2023-09-13 11:30:11 +0200

    Update

--- Algorithms/GroverCurso.py
+++ Algorithms/GroverCurso.py
@@ -1,6 +1,6 @@
 from numpy import *
 import math
-from qat.lang.AQASM import *
+from qat.lang.AQASM import Program, H, X, Z, QRoutine, CustomGate
 from qat.qpus import get_default_qpu
 
 qubits = 4
@@ -18,23 +18,26 @@ for num in range(numero):
 
 print(lista)
 
-matriz = zeros((numero,numero))
-print(matriz)
+matriz = zeros((numero,numero)) #Inicializamos la matriz con ceros de tamaño numero x numero
+print(matriz) #Imprimimos la matriz de todo 0
 
-for i in range(len(matriz)):
-    if i in lista:
-        matriz[i][i] = -1
+for i in range(len(matriz)): #Recorremos la diagonal de la matriz
+    if i in lista: #Comprobamos si el numero es primo comrpobando si esta en la lista
+        matriz[i][i] = -1 #Si es primo cambiamos a -1
     else: 
-        matriz[i][i] = 1
+        matriz[i][i] = 1 #Si no es primo no cambiamos a 1
 
-print(matriz)
+matriz_primos = CustomGate(matriz) #Creamos una puerta personalizada con la matriz para poder operar
+print(matriz) #Imprimimos la matriz con los valores cambiados
 
-def oraculo(k, matriz):
+#Definimos el oracle 
+def oraculo(k, matriz_primos):
     routine = QRoutine()
     wires = routine.new_wires(k)
-    matriz(wires)
+    matriz_primos(wires)
     return routine
 
+#Definimos el difusor
 def diffusor(k):
     routine = QRoutine()
     wires = routine.new_wires(k)
@@ -47,11 +50,35 @@ def diffusor(k):
     return routine
 
 numeroSoluciones = len(lista)
+print("Numero de soluciones: %d" % numeroSoluciones)
 numeroIteraciones = int(math.pi / (4 * arccos(sqrt(1 - numeroSoluciones / numero))))
 print("Numero de iteraciones: %d" % numeroIteraciones)
 
-probabilidad = sin((2 * numeroIteraciones + 1) * arccos(sqrt((numero - numeroSoluciones)/ numero)))**2
+probabilidad = sin((2 * numeroIteraciones + 1) * arccos(sqrt((numero - numeroSoluciones) / numero)))**2
 print("Probabilidad de encontrar la solucion: %f" %probabilidad)
+
+qprog = Program()
+nqbits = qprog.qalloc(qubits)
+
+difusor = diffusor(qubits)
+oracle = oraculo(qubits, matriz_primos)
+
+for qbit in nqbits:
+    H(qbit)
+
+for _ in range(numeroIteraciones):
+    oracle(nqbits)
+    difusor(nqbits)
+    
+circuit = qprog.to_circ()
+job = circuit.to_job()
+result = get_default_qpu().submit(job)
+
+for sample in result:
+    print("State %s probability %s" % (sample.state, sample.probability))
+
+circuit.display()
+
 
 
 
