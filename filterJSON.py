99b862e3e543880ee6de5e82e7708b313e24e272
arturo <arturequejo@gmail.com>
2023-09-15 10:10:59 +0200

    Update

--- filterJSON.py
+++ filterJSON.py
@@ -8,79 +8,121 @@ with open(ruta_archivo, "r") as archivo:
 
 data = json.loads(data)
 
-<<<<<<< HEAD
-archivo = './data.csv'
+archivo = "archivo.csv"
+medias = "medias.csv"
 
+datos_t1 = []
+datos_t2 = []
+datos_readout_error = []
+datos_readout_length = []
+datos_prob_01 = []
+datos_prob_10 = []
 
-for i in data["qubits"]:
-    if(i[0]['name'] == 'T1'):
-        datos_t1 = i[0]['value']
-        data = {
-            'T1': [datos_t1]
-        }
-        df = pd.DataFrame(data)
-        print(df)
-        df.to_csv(archivo, index=False)
+datos_rz1 = []
+datos_rz2 = []
 
-        
+datos_x1 = []
+datos_x2 = []
 
+datos_sx1 = []
+datos_sx2 = []
 
-
-    
-
-
-
-
+datos_cnot1 = []
+datos_cnot2 = []
 
 for i in data["qubits"]:
-    datos_t2 = i[1]
+   if i[0]['name'] == 'T1':
+       datos_t1.append(i[0]['value'])
+
+for i in data["qubits"]:
+    if i[1]['name'] == 'T2':
+        datos_t2.append(i[1]['value'])
+
+for i in data["qubits"]:
+    if i[4]['name'] == 'readout_error':
+        datos_readout_error.append(i[4]['value'])
+
+for i in data["qubits"]:
+    if i[7]['name'] == 'readout_length':
+        datos_readout_length.append(i[7]['value'])
+
+for i in data["qubits"]:
+    if i[5]['name'] == 'prob_meas0_prep1':
+        datos_prob_01.append(i[5]['value'])
+
+for i in data["qubits"]:
+    if i[6]['name'] == 'prob_meas1_prep0':
+        datos_prob_10.append(i[6]['value'])
 
 for i in data["gates"]:
     if(i['gate'] == 'rz'):
-         datos_rz = i
-
+        datos_rz1.append(i['parameters'][0]['value'])
+        datos_rz2.append(i['parameters'][1]['value'])
+         
 for i in data["gates"]:
     if(i['gate'] == 'x'):
-        datos_x = i
-            
+        datos_x1.append(i['parameters'][0]['value'])
+        datos_x2.append(i['parameters'][1]['value'])
+
 for i in data["gates"]:
     if(i['gate'] == 'sx'):
-        datos_sx = i
+        datos_sx1.append(i['parameters'][0]['value'])
+        datos_sx2.append(i['parameters'][1]['value'])
 
 for i in data["gates"]:
     if(i['gate'] == 'cx'):
-        datos_cnot = i
-=======
-archivo_t1 = "archivo_t1.txt"
+        datos_cnot1.append(i['parameters'][0]['value'])
+        datos_cnot2.append(i['parameters'][1]['value'])
 
-datos_t1 = {}
+print(datos_prob_01)
 
+#Realizar la media cada variable
+mediaT1 = sum(datos_t1) / len(datos_t1)
+mediaT2 = sum(datos_t2) / len(datos_t2)
+mediaReadoutError = sum(datos_readout_error) / len(datos_readout_error)
+mediaReadoutLength = sum(datos_readout_length) / len(datos_readout_length)
+mediaProb01 = sum(datos_prob_01) / len(datos_prob_01)
+mediaProb10 = sum(datos_prob_10) / len(datos_prob_10)
+mediaRz = (sum(datos_rz1) + sum(datos_rz2)) / len(datos_rz1)
+mediaX = (sum(datos_x1) + sum(datos_x2)) / len(datos_x1)
+mediaSx = (sum(datos_sx1) + sum(datos_sx2)) / len(datos_sx1)
+mediaCnot = (sum(datos_cnot1) + sum(datos_cnot2)) / len(datos_cnot1)
 
-for i in data["qubits"]:   
-    if(i[0]['name'] == 'T1'):
-        datos_t1 = i[0]
-        with open(archivo_t1, "w") as archivo:
-            archivo.write(str(datos_t1))
-        
-def filterJSONT2(data):
-    for i in data["qubits"]:
-       datos_t2 = i[1]
+data = {
+    'T1': datos_t1,
+    'T2': datos_t2,
+    'Readout error': datos_readout_error,
+    'Readout lenght': datos_readout_length,
+    'Prob 01': datos_prob_01,
+    'Prob 10': datos_prob_10,
+    'Rz gate error': datos_rz1,
+    'Rz gate lenght': datos_rz2,
+    'X gate error': datos_x1,
+    'X gate lenght': datos_x2,
+    'Sx gate error': datos_sx1,
+    'Sx gate lenght': datos_sx2,
 
-def filterJSONRz(data):
-    for i in data["gates"]:
-        if(i['gate'] == 'rz'):
-            datos_rz = i
-        elif (i['gate'] == 'x'):
-            datos_x = i
-        elif(i['gate'] == 'sx'):
-            datos_sx = i
-        elif(i['gate'] == 'cx'):
-            datos_cnot = i
+}
 
+df = pd.DataFrame(data)
+print(df)
+df.to_csv(archivo, index=False)
 
-filterJSONT2(data)
-filterJSONRz(data)
->>>>>>> 6c3146294f99d9985d13a51dc945cddfcaf4e2a8
+data2 = {
+    'T1': mediaT1,
+    'T2': mediaT2,
+    'Readout error': mediaReadoutError,
+    'Readout lenght': mediaReadoutLength,
+    'Prob 01': mediaProb01,
+    'Prob 10': mediaProb10,
+    'Rz': mediaRz,
+    'X': mediaX,
+    'Sx': mediaSx,
+    'Cnot': mediaCnot,
+}
 
+df2 = pd.DataFrame(data2, index=[0])
+print(df2)
+df2.to_csv(medias, index=False)
 
 
