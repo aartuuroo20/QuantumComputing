f51123dbe873625eeb93b0cf2434e3b1f83cf3bf
arturo <arturequejo@gmail.com>
2023-10-11 11:19:30 +0200

    Update

--- FeatureMap/ZZFeatureMap.py
+++ FeatureMap/ZZFeatureMap.py
@@ -3,20 +3,16 @@ import numpy as np
 from qat.lang.AQASM import Program, H, X, PH
 from DataSet import DataSet
 
-class Circuit():
+class ZZFeatureMap():
     #Constructor
     def __init__(self):
-        self.nqubits = 2
+        nqubits = 2
         self.qprogram = Program()
-        self.qubits = self.qprogram.qalloc(self.nqubits)
-
+        self.qubits = self.qprogram.qalloc(nqubits)
         self.CreateZZFeatureMap()
-        self.X()
-        self.DisplayCircuit()
 
     #Function that create the ZZFeatureMap
     def CreateZZFeatureMap(self):
-
         H(self.qubits[0])
         H(self.qubits[1])
 
@@ -24,9 +20,6 @@ class Circuit():
         PH(2*(np.pi - DataSet().X[0][0]) * (np.pi - DataSet().X[0][1]))(self.qubits[1])
         H(self.qubits[1])
         X(self.qubits[1])
-
-    def X(self):
-        X(self.qubits[0])
 
     #Function that display the circuit
     def DisplayCircuit(self):
--- FeatureMap/main.py
+++ FeatureMap/main.py
@@ -1,11 +1,37 @@
-from qat.lang.AQASM import Program
-from DataSet import DataSet
-from Circuit import Circuit
+import numpy as np
 
+from qat.lang.AQASM import Program, H, X, CustomGate
+from DataSet import DataSet
+from ZZFeatureMap import ZZFeatureMap
+from VariationalCircuit import VariationalCircuit
+
+'''
 dataset = DataSet()
 dataset.Draw()
 
-circuit = Circuit()
+program = Program()
+nqubits = 2
+qubits = program.qalloc(nqubits)
+
+X(qubits[0])
+zzmf = ZZFeatureMap()
+customGate = CustomGate(zzmf)
+
+
+circuit = program.to_circ()
+circuit.display()
+
+'''
+
+vc = VariationalCircuit()
+vc.DisplayCircuit()
+
+
+
+
+
+
+
 
 
 
