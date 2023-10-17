ccf7733cdd842825a5a0c980922642c80212d18a
arturo <arturequejo@gmail.com>
2023-10-17 13:30:53 +0200

    Update

--- Data Enconding/ZZFeatureMap/mainAux.py
+++ Data Enconding/ZZFeatureMap/mainAux.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from qat.lang import Program, H, X, PH, RY, CSIGN, RZ
+from qat.core import Variable
 
 #Creation of the dataset
 num_samples = 20
@@ -35,29 +36,35 @@ H(qubits[1])
 X(qubits[1])
 
 #Creation of variational circuit
-'''
-theta = []
-for i in range(12):
-    theta.append(qprogram.new_var(float, '\\theta'))
-'''
-theta = qprogram.new_var(float, '\\theta')
 
-RY(theta)(qubits[0])
-RY(theta)(qubits[1])
-RZ(theta)(qubits[0])
-RZ(theta)(qubits[1])
+var0 = Variable("var0")
+var1 = Variable("var1")
+var2 = Variable("var2")
+var3 = Variable("var3")
+var4 = Variable("var4")
+var5 = Variable("var5")
+var6 = Variable("var6")
+var7 = Variable("var7")
+var8 = Variable("var8")
+var9 = Variable("var9")
+var10 = Variable("var10")
+var11 = Variable("var11")
+
+RY(var0)(qubits[0])
+RY(var1)(qubits[1])
+RZ(var2)(qubits[0])
+RZ(var3)(qubits[1])
 CSIGN(qubits[0], qubits[1])
-RY(theta)(qubits[0])
-RY(theta)(qubits[1])
-RZ(theta)(qubits[0])
-RZ(theta)(qubits[1])
+RY(var4)(qubits[0])
+RY(var5)(qubits[1])
+RZ(var6)(qubits[0])
+RZ(var7)(qubits[1])
 CSIGN(qubits[0], qubits[1])
-RY(theta)(qubits[0])
-RY(theta)(qubits[1])
-RZ(theta)(qubits[0])
-RZ(theta)(qubits[1])
+RY(var8)(qubits[0])
+RY(var9)(qubits[1])
+RZ(var10)(qubits[0])
+RZ(var11)(qubits[1])
 
 #Creation of the circuit and display it
 circuit = qprogram.to_circ()
 circuit.display()
-
