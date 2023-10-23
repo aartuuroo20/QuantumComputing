f4a975922056b17910d48a711db1e17aef409026
arturo <arturequejo@gmail.com>
2023-10-23 12:53:54 +0200

    Update

--- DataEncoding/BasisEncoding/main.py
+++ DataEncoding/BasisEncoding/main.py
@@ -1,25 +1,33 @@
 import numpy as np
 
-from qat.lang import Program, AbstractGate
+from qat.lang import Program, AbstractGate, CustomGate
 from qat.qpus import get_default_qpu
+from qat.interop.qiskit import qlm_to_qiskit
 from matrix import Matrix
 
-matrix = Matrix()
+def genMatrix():
+    a = 1/np.sqrt(2)
+
+    matrix = np.array([[a,0,0,0],
+              [0,1.,0,0],
+              [a,0,1.,0],
+              [0,0,0,1.]])
+        
+    def gram_schmidt_columns(X):
+        Q, R = np.linalg.qr(X)
+        return Q
+    
+    matrix_aux = gram_schmidt_columns(matrix)
+    return matrix_aux
 
 qprogram = Program()
 nqubits = 2
 qubits = qprogram.qalloc(nqubits)
 
-basis = AbstractGate("basis", [], arity=2, matrix_generator=matrix.genMatrix())
+basis = AbstractGate("basis", [], arity=2, matrix_generator=genMatrix())
 basis()(qubits[0], qubits[1])
 
-circuit = qprogram.to_circ(include_matrices=True)
-job = circuit.to_job()
-result = get_default_qpu().submit(job)
-
-for sample in result:
-    print("State %s probability %s" % (sample.state, sample.probability))
-
+circuit = qprogram.to_circ()
 circuit.display()
 
 
---
+++ DataEncoding/ZZFeatureMap/Mode 2/main.py
@@ -0,0 +1,82 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from qat.lang import Program, H, PH, CNOT, qrout, RY
+from qat.core import Variable
+
+from DataSet import DataSet
+
+class Circuit:
+    #Constructor that initialize the circuit with 2 qubits and create the qprogram
+    def __init__(self):
+        nqubits = 2
+        self.qprogram = Program()
+        self.qubits = self.qprogram.qalloc(nqubits)
+
+    #Function that create the ZZFeatureMap and apply it to the qprogram
+    def ZZFeatureMap(self):
+        ListVarData = []
+        for i in range(2):
+            ListVarData.append(Variable("varData" + str(i)))
+
+        @qrout
+        def zzmf():
+            H(0)
+            H(1)
+            PH(2* ListVarData[0])(0)
+            PH(2* ListVarData[1])(0)
+            CNOT(0, 1)
+            PH(2*(np.pi - ListVarData[0]) * (np.pi - ListVarData[1]))(1)
+            CNOT(0, 1)
+            H(0)
+            H(1)
+            PH(2* ListVarData[0])(0)
+            PH(2* ListVarData[1])(0)
+            CNOT(0, 1)
+            PH(2*(np.pi - ListVarData[0]) * (np.pi - ListVarData[1]))(1)
+            CNOT(0, 1)
+        
+        self.qprogram.apply(zzmf, self.qubits)
+
+    #Function that create the variational circuit and apply it to the qprogram
+    def varCircuit(self):
+        ListVarTheta = []
+        for i in range(8):
+            ListVarTheta.append(Variable("varTheta" + str(i)))
+
+        @qrout
+        def varcircuit():
+            RY(ListVarTheta[0])(0)
+            RY(ListVarTheta[1])(1)
+            CNOT(0, 1)
+            RY(ListVarTheta[2])(0)
+            RY(ListVarTheta[3])(1)
+            CNOT(0, 1)
+            RY(ListVarTheta[4])(0)
+            RY(ListVarTheta[5])(0)
+            CNOT(0, 1)
+            RY(ListVarTheta[6])(0)
+            RY(ListVarTheta[7])(1)
+        
+        self.qprogram.apply(varcircuit, self.qubits)
+
+    #Function that display the circuit
+    def display(self):
+        circuit = self.qprogram.to_circ()
+        circuit.display()
+
+    #Function that return the circuit
+    def circuit(self):
+        return self.qprogram.to_circ()
+
+circuit = Circuit()
+circuit.ZZFeatureMap()
+circuit.varCircuit()
+circuit.display()
+
+dataset = DataSet()
+dataset.Draw()
+
+    
+
+
